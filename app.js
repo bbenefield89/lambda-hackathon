6361d58b10d788fdd10347f15c762264db81c4bf
bbenefield89 <bsquared18@gmail.com>
2018-06-26 18:45:51 -0400

    Initial push

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log(`App listening on ${ port }`));
